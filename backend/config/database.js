bcc1b66f0afa2fc043554a1133184492bf16b2a6
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2023-09-25 08:55:47 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,42 +1,28 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// exports.connectDatabase = () => {
-//   mongoose
-//     .connect(process.env.MONGO_URI)
-//     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
-//     .catch((err) => console.log(err));
-// };
+exports.connectDatabase = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((con) => console.log(`Database Connected: ${con.connection.host}`))
+    .catch((err) => console.log(err));
+};
  
 
-const mongoose = require("mongoose");
-const connectDatabase = async () => {
-  try {
-     //const conn = await mongoose.connect(`mongodb://localhost:27017/SocialMedia`, {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected on ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Failed to connect to MongoDB: ${error.message}`);
-  }
-};
- module.exports = connectDatabase;
-
-
+// const mongoose = require("mongoose");
 // const connectDatabase = async () => {
 //   try {
+//      //const conn = await mongoose.connect(`mongodb://localhost:27017/SocialMedia`, {
 //     const conn = await mongoose.connect(process.env.MONGO_URI, {
 //       useNewUrlParser: true,
 //       useUnifiedTopology: true,
 //     });
-//     if (conn) {
-//       console.log(`MongoDB Connected on ${conn.connection.host}`);
-//     } else {
-//       console.log("Failed to connect DB");
-//     }
+//     console.log(`MongoDB Connected on ${conn.connection.host}`);
 //   } catch (error) {
-//     console.log(`Failed with error: ${error.message}`);
+//     console.error(`Failed to connect to MongoDB: ${error.message}`);
 //   }
 // };
+//  module.exports = connectDatabase;
+
+
+ 
 
